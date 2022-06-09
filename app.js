f1e2f21e5c2173565fa2ed7fd91819c4afdd6c52
Cristian Riaño <albeirochunza90@gmail.com>
2022-06-08 22:24:30 -0500

    Conectado a la base de datos MongoDB junto con js html

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
 require('dotenv').config()
 
 const express= require('express')
+const { default: mongoose } = require('mongoose')
+const path= require('path')
+const { nextTick } = require('process')
+const { stringify } = require('querystring')
+
 const app= express()
+mongoose
+    .connect(
+        `mongodb+srv://Crisch:${process.env.MONGO_DB_PASS}@development.w1ujevh.mongodb.net/${process.env.nombre_Base_Datos}?retryWrites=true&w=majority`
+        )
+    .then((result)=>{
+        app.listen(PORT,()=>{
+            console.log(`Servidor escuchando el el puerto ${PORT}`)
+        })
+        console.log('conexion exitosa a la base de datos')
+    })
+    .catch((err)=>console.log(err))
+const productSchema=mongoose.Schema(
+    {
+    name:{string, required: true},
+    price:Number,
+    },
+    { timestamps: true }
+
+)
+
+const Product=mongoose.model('product', productSchema)
+
+
+app.use(express.json())
+app.post('/api/v1/products',(req, res, next)=>{
+    const newProduct= new Product(req.body)
+    new Product
+        .Save()
+        .then((result)=>{
+            res.status(201).json({ok:true})
+        })
+        .catch((err)=> console.log(err))
+
+        
+    res.status(201).json({ok:true})
+})
 
 app.get('/',  (req , res)=>{
     console.log('Peticion recibida')
-    res.send('<h1>hola mundo <br>Soy Cristian Albeiro Riaño Chunza</br></h1><p>Ingeniero de sistemas Estudiando en UNAD</p>')
+    next()    
 })
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 
 const PORT= process.env.PORT || 4000
 
-app.listen(PORT,()=>{
-    console.log(`Servidor escuchando el el puerto ${PORT}`)
-})
+
+
