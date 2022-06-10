31364b5a46af7e8630b840b61fb9d2dc0dbf9117
Cristian Riaño <albeirochunza90@gmail.com>
2022-06-09 20:45:14 -0500

    consultando apis de pokemon tanto desde el servidor como del cliente

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express= require('express')
 const { default: mongoose } = require('mongoose')
 const path= require('path')
-const { next } = require('process')
+const axios=require('axios')
 
 
 const app= express()
@@ -29,11 +29,6 @@ const productSchema=mongoose.Schema(
 
 const Product=mongoose.model('Product', productSchema)
 
-app.get('/',  (req , res,next)=>{
-    console.log('Peticion recibida')
-    next();
-})
-
 
 app.use(express.json())
 app.post('/api/v1/products', (req, res, next)=>{
@@ -48,6 +43,49 @@ app.post('/api/v1/products', (req, res, next)=>{
 
         
     res.status(201).json({ok:true})
+    next()
+})
+
+
+
+app.get('/',  (req , res)=>{
+    console.log('Peticion recibida')
+    const pokeApiBaseUrl='https://pokeapi.co/api/v2/pokemon'
+    const pokemonABuscar='charmeleon'
+    // .then() sirve como funcion que se debe ejecutar si todo sale bien, y el .catch sirve como funcion si ocurre algun error
+    axios(`${pokeApiBaseUrl}/${pokemonABuscar}`)
+        .then((axiosResponse)=>{
+        
+        const pokemon=axiosResponse.data
+        console.log({pokemon})
+        const html=`
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Learning backend and frontend</title>
+            <link rel="stylesheet" href="style.css">
+            <script src="index.js" defer></script>
+            <!-- defer  hace que el codigo javascript se ejecute al final -->
+        </head>
+        <body>
+            <h1 class="h1">
+            hola mundo <br>Soy Cristian Albeiro Riaño Chunza</br>
+            </h1>
+            <p>Ingeniero de sistemas Estudiando en UNAD</p>
+            <div class="form-container">
+                <input type="text" id="productName" placeholder="Nombre del producto">
+                <input type="number" id="productPrice" placeholder="Precio del producto">
+                <button>Crear producto</button>
+            </div>
+            <div class="poke-card">
+                <h3>${pokemon.name} traido con Server render <h3>
+                <img src=${pokemon.sprites.front_default}  alt="imagen de ${pokemon.name}">
+            </div>
+        </body>
+        `
+        res.send(html)
+        })
 })
 
 
--- public/index.js
+++ public/index.js
@@ -2,6 +2,30 @@ const inputName=document.querySelector('#productName')
 const inputPrice=document.querySelector('#productPrice')
 
 const button=document.querySelector('button')
+
+const pokeApiBaseUrl='https://pokeapi.co/api/v2/pokemon'
+const pokemonABuscar='charmander'
+// .then() sirve como funcion que se debe ejecutar si todo sale bien, y el .catch sirve como funcion si ocurre algun error
+fetch(`${pokeApiBaseUrl}/${pokemonABuscar}`)
+    .then((res)=>{
+    console.log({respuesta: res})
+    return res.json()
+    }).then((pokemon)=>{
+        console.log({pokemon})
+        const html=`
+        <h3>${pokemon.name} traido desde Client Render<h3>
+        <img src="${pokemon.sprites.front_default}"  alt="imagen de ${pokemon.name}">
+        `
+
+        const div = document.createElement('div')
+        div.classList.add('poke-card')
+        div.innerHTML=html
+        
+        document.querySelector('body').appendChild(div)
+    })
+    .catch()    
+
+
 button.addEventListener('click',(e)=>{
     
     const name=inputName.value
